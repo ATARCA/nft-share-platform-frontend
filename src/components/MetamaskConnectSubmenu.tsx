a3b46ddea5df9b53b07c958298c07141f6bc0252
Martin Moravek <martin.m.moravek@gmail.com>
2022-02-24 16:14:30 +0100

    remove ens - not supported on xDai

--- src/components/MetamaskConnectSubmenu.tsx
+++ src/components/MetamaskConnectSubmenu.tsx
@@ -112,7 +112,6 @@ const Status = ({connector}: { connector: Connector }) => {
 const Accounts = () => {
     const provider = useProvider()
     const accounts = useAccounts()
-    const ENSNames = useENSNames(provider)
     const chainId = useChainId()
     const isActive = useIsActive()
 
@@ -132,7 +131,7 @@ const Accounts = () => {
                             ? ' None'
                             : shortenedAccounts?.map((account, i) => (
                                 <ul key={account} style={{ margin: 0, padding:0, overflow: 'hidden', textOverflow: 'ellipsis' }}>
-                                    <b>{ENSNames?.[i] ?? account}</b>
+                                    <b>{account}</b>
                                     {balances?.[i] ? ` (${formatEther(balances[i])} ${getCurrencySymbol(chainId)})` : null}
                                 </ul>
                             ))}
