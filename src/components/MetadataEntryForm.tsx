f557263cb79ae32f458d0883b03c0620da18c101
Martin Moravek <martin.m.moravek@gmail.com>
2022-05-10 15:09:00 +0200

    add uuid

--- src/components/MetadataEntryForm.tsx
+++ src/components/MetadataEntryForm.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react"
 import React from "react";
 import { Icon, Input, Header, Button } from "semantic-ui-react";
+import { v4 as uuidv4 } from 'uuid';
 
 const MetadataEntryItem = ({
     propertyName, 
@@ -32,44 +33,44 @@ const MetadataEntryItem = ({
     )
 };
 
-const twitterContributionPropertiesTemplate: [string, string][] = [[  'Author', ''], ['Topic', ''], ['Contribution URI', 'http://']]
-const eventOrganiserContributionPropertiesTemplate: [string, string][] = [['Organizer', ''], ['Event Name', ''], ['Event date', ''], ['Event location', '']]
+const twitterContributionPropertiesTemplate: [string, string, string][] = [[ uuidv4(), 'Author', ''], [ uuidv4(), 'Topic', ''], [ uuidv4(), 'Contribution URI', 'http://']]
+const eventOrganiserContributionPropertiesTemplate: [string, string, string][] = [[ uuidv4(), 'Organizer', ''], [ uuidv4(), 'Event Name', ''], [ uuidv4(), 'Event date', ''], [ uuidv4(), 'Event location', '']]
 
 export const MetadataEntryForm = () => {
 
     const [ tokenName, setTokenName ] = useState('')
     const [ tokenDescription, setTokenDescription ] = useState('')
 
-    const [ propertiesToValuesArray, setPropertiesToValuesArray ] = useState<[string, string][]>([])
+    const [ propertiesToValuesArray, setPropertiesToValuesArray ] = useState<[string, string, string][]>([])
 
-    const updatePropertyValue = (index: number, newValue:string) => {
+    const updatePropertyValue = (uuid: string, newValue:string) => {
         const arrayCopy = [...propertiesToValuesArray]
-        arrayCopy.map(((entry, i) => {if (i === index) entry[1] = newValue; return entry}))
+        arrayCopy.map(( entry => {if (uuid === entry[0]) entry[2] = newValue; return entry}))
         setPropertiesToValuesArray(arrayCopy)
     }
 
     console.log('current map', propertiesToValuesArray)
 
-    const updatePropertyName = (index: number, newPropertyName:string) => {
+    const updatePropertyName = (uuid: string, newPropertyName:string) => {
         const arrayCopy = [...propertiesToValuesArray]
-        arrayCopy.map(((entry, i) => {if (i === index) entry[0] = newPropertyName; return entry}))
+        arrayCopy.map(( entry => {if (uuid === entry[0]) entry[1] = newPropertyName; return entry}))
         setPropertiesToValuesArray(arrayCopy)
     }
 
-    const demoContent: [string, string][] = [['a','1'],['b','2'],['c','3']];
+    const demoContent: [string, string, string][] = [[ uuidv4(), 'a','1'],[ uuidv4(), 'b','2'],[ uuidv4(), 'c','3']];
 
     if (propertiesToValuesArray.length === 0) {
         setPropertiesToValuesArray(demoContent)//TODO remove
     }
 
-    const removeProperty = (index: number) => {
-        const filteredArray = propertiesToValuesArray.filter(((entry, i) => (i !== index)))        
+    const removeProperty = (uuid: string) => {
+        const filteredArray = propertiesToValuesArray.filter(((entry) => (uuid !== entry[0])))        
         setPropertiesToValuesArray(filteredArray)
     }
 
     const addEmptyProperty = () => {
         const arrayCopy = [...propertiesToValuesArray]
-        arrayCopy.push(['',''],)
+        arrayCopy.push([ uuidv4(), '',''],)
         setPropertiesToValuesArray(arrayCopy)
     }
     return (
@@ -81,21 +82,22 @@ export const MetadataEntryForm = () => {
                 Properties
             </Header>
             <text>Choose template</text>
-            <Button basic onClick={() => { setPropertiesToValuesArray([]); setPropertiesToValuesArray(twitterContributionPropertiesTemplate)}}>Twitter contribution</Button>
-            <Button basic onClick={() => { setPropertiesToValuesArray([]); setPropertiesToValuesArray(eventOrganiserContributionPropertiesTemplate)}}>Event organiser</Button>
+            <Button basic onClick={() => { setPropertiesToValuesArray(twitterContributionPropertiesTemplate)}}>Twitter contribution</Button>
+            <Button basic onClick={() => { setPropertiesToValuesArray(eventOrganiserContributionPropertiesTemplate)}}>Event organiser</Button>
             {Array.from( propertiesToValuesArray ).map( (value, index) => { 
-                const propertyName = value[0]
-                const propertyValue = value[1]
+                const uuid = value[0]
+                const propertyName = value[1]
+                const propertyValue = value[2]
 
                 return (
                     <MetadataEntryItem
                         style={{margin: '10px 0px 0px 10px'}}
-                        key={index} 
+                        key={uuid} 
                         propertyName={propertyName} 
                         propertyValue={propertyValue}
-                        onRemoveClicked={() => removeProperty(index)}
-                        onPropertyNameChanged={(newName) => updatePropertyName(index, newName) }
-                        onPropertyValueChanged={(newValue => updatePropertyValue(index, newValue))}/>)})}
+                        onRemoveClicked={() => removeProperty(uuid)}
+                        onPropertyNameChanged={(newName) => updatePropertyName(uuid, newName) }
+                        onPropertyValueChanged={(newValue => updatePropertyValue(uuid, newValue))}/>)})}
             <Icon name='plus' circular onClick={addEmptyProperty} style={{margin: '10px'}}
             />
 
