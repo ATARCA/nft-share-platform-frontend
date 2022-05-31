383fc87f77a5f01724f0f2a8019a490e8e367790
Martin Moravek <martin.m.moravek@gmail.com>
2022-05-31 13:31:35 +0200

    Style buttons - normal, primary, disabled

--- src/components/ConsentPanel.tsx
+++ src/components/ConsentPanel.tsx
@@ -53,7 +53,7 @@ const ConsentPanelContent = () => {
         <Menu fluid vertical>
             <Menu.Item>
                 <p>You need to give consent before using the platform. TODO complete text.</p>
-                <Button color='green' onClick={onGiveConsentClicked}>Sign consent</Button>
+                <Button primary onClick={onGiveConsentClicked}>Sign consent</Button>
                 { uploadConsentResultError ? <Message error>
                     <Message.Header>Consent upload failed</Message.Header>
                     <p>{uploadConsentResultError}</p>
