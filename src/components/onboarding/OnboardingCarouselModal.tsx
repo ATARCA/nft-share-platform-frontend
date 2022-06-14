a558da7793fb36a517e9cd3b410bb72271fd2c43
Martin Moravek <martin.m.moravek@gmail.com>
2022-06-14 17:00:33 +0200

    add close button on last slide

--- src/components/onboarding/OnboardingCarouselModal.tsx
+++ src/components/onboarding/OnboardingCarouselModal.tsx
@@ -1,11 +1,10 @@
-import React, { useState } from "react";
-import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup } from 'pure-react-carousel';
+import React, { useContext, useEffect, useState } from "react";
+import { CarouselProvider, Slider, Slide, ButtonBack, ButtonNext, DotGroup, CarouselContext } from 'pure-react-carousel';
 import 'pure-react-carousel/dist/react-carousel.es.css';
-import { Button, Container, Grid, Icon, Image, Modal, Rail, Segment } from "semantic-ui-react";
+import { Button, Container, Icon, Image, Modal } from "semantic-ui-react";
 import guitar1 from '../../images/guitar1.jpg';
 import guitar2 from '../../images/guitar2.jpg';
 import guitar3 from '../../images/guitar3.jpg';
-import useCookie from 'react-use-cookie';
 import { useTutorialCompletedCookie } from "../../hooks/hooks";
 
 interface CarouselEntry {
@@ -18,37 +17,50 @@ const carouselContent : CarouselEntry[] =
         {image: guitar2, text: 'I am the second Slide.'},
         {image: guitar3, text: 'I am the third Slide.'}]
 
-const OnboardingCarousel = () => {
+const OnboardingCarousel = ( { onCloseClicked } : {onCloseClicked: () => void}) => {
+
+    const carouselContext = useContext(CarouselContext);
+    const [currentSlide, setCurrentSlide] = useState(carouselContext.state.currentSlide);
+    useEffect(() => {
+        function onChange() {
+            setCurrentSlide(carouselContext.state.currentSlide);
+        }
+        carouselContext.subscribe(onChange);
+        return () => carouselContext.unsubscribe(onChange);
+    }, [carouselContext]);
+
+    const isLastSlide = currentSlide !== carouselContent.length -1
 
     return (
         <Container textAlign="center" text>
-            <CarouselProvider
-                naturalSlideWidth={1.35}
-                naturalSlideHeight={1}
-                totalSlides={3}>
-                <Slider>
-                    {carouselContent.map( (item,i) =>     
-                        <Slide index={i} key={i}>
-                            <Image src={item.image}/>
-                            {item.text}
-                        </Slide>
-                    )}
-                </Slider>
+           
+            <Slider>
+                {carouselContent.map( (item,i) =>     
+                    <Slide index={i} key={i}>
+                        <Image src={item.image}/>
+                        {item.text}
+                    </Slide>
+                )}
+            </Slider>
 
-                <DotGroup />
+            <DotGroup />
                
-                <Button as={ButtonBack}><Icon name="arrow left"/></Button>
+            <Button as={ButtonBack}><Icon name="arrow left"/></Button>
+            
+            { isLastSlide ? 
                 <Button as={ButtonNext}>Next <Icon name="arrow right"/></Button>
-            </CarouselProvider>
+                :
+                <Button onClick={onCloseClicked}>Close</Button>
+            }
+            
         </Container>
     )
 }
 
-//TODO next cookie value
-
 const OnboardingCarouselModal = () => {
 
     const [tutorialCompleted, setTutorialCompleted] = useTutorialCompletedCookie();
+
     return (
         <Modal
             closeIcon
@@ -56,7 +68,12 @@ const OnboardingCarouselModal = () => {
            
             open={!tutorialCompleted}>
 
-            <OnboardingCarousel/>
+            <CarouselProvider
+                naturalSlideWidth={1.35}
+                naturalSlideHeight={1}
+                totalSlides={3}>
+                <OnboardingCarousel onCloseClicked={() => setTutorialCompleted(true)}/>
+            </CarouselProvider>
 
         </Modal>
     )
