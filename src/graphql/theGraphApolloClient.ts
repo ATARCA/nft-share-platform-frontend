17e329f32e1699a77e4e4719c8ff9977d110398f
mmartinmo <41465214+mmartinmo@users.noreply.github.com>
2022-08-29 16:41:45 +0900

    subgraph url as env parameter (#39)

--- src/graphql/theGraphApolloClient.ts
+++ src/graphql/theGraphApolloClient.ts
@@ -3,6 +3,6 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 export const theGraphApolloClient = new ApolloClient({
     cache: new InMemoryCache(),
     link: new HttpLink({
-        uri: 'https://api.thegraph.com/subgraphs/name/atarca/nft-share-platform-goerli',
+        uri: process.env.REACT_APP_SUBGRAPH_URI || 'https://api.thegraph.com/subgraphs/name/atarca/nft-share-platform-goerli',
     })
 })
