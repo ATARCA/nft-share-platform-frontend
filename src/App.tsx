51cd13aebc4c346c39325af67e643aad61da906f
Martin Moravek <martin.m.moravek@gmail.com>
2022-09-23 12:35:11 +0200

    fix main menu on mobile

--- src/App.tsx
+++ src/App.tsx
@@ -25,7 +25,7 @@ function App() {
     return (
         <div className="App">
             <Router>
-                <Menu secondary>
+                <Menu secondary stackable={true}>
                     <HomeMenuButtons/>
                     <MetamaskConnectSubMenu/>    
                     <MainMenuWalletDropdown/>
