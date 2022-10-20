f575b9234969d059e6b990242eb645c87a4e66ff
Martin Moravek <martin.m.moravek@gmail.com>
2022-10-20 14:45:33 +0200

    remove carousel + update package-lock.json

--- src/components/topLevel/Home.tsx
+++ src/components/topLevel/Home.tsx
@@ -4,7 +4,6 @@ import { GET_TOKENS } from '../../queries-thegraph/queries';
 import { theGraphApolloClient } from '../../graphql/theGraphApolloClient';
 import { defaultErrorHandler } from '../../graphql/errorHandlers';
 import TokenGrid from '../../components/TokenGrid';
-import OnboardingCarousel from '../onboarding/OnboardingCarousel';
 import { TokensQuery, TokensQueryVariables } from '../../queries-thegraph/types-thegraph/TokensQuery';
 import { Header } from 'semantic-ui-react';
 import { Link, useNavigate } from 'react-router-dom';
@@ -26,7 +25,6 @@ const Home = () => {
 
     return (
         <div>
-            <OnboardingCarousel/>
             <div style={{'textAlign': 'left', padding: '5vh 10vw 7vh 10vw'}}>
                 <Header className="No-overflow" as='h1'>Contribution awards to the Streamr community</Header>
                 <p style={{ padding: '3vh 30vw 3vh 0vw'}}>Streamr community award tokens (sNFT’s) are minted to community members to acknowledge their valuable contributions to the project and community. Read more <Link to={aboutRoute} onClick={() => navigate(aboutRoute)}>about this service.</Link></p>
