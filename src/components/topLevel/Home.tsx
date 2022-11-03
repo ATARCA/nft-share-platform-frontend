54dfbf41d76d6d77d855b505fbe5bef9da8cb23f
Jarno <jarno.marttila@sujuwa.fi>
2022-11-03 15:31:56 +0100

    Social media links & project link to project page

--- src/components/topLevel/Home.tsx
+++ src/components/topLevel/Home.tsx
@@ -6,11 +6,13 @@ import { defaultErrorHandler } from '../../graphql/errorHandlers';
 import TokenGrid from '../../components/TokenGrid';
 import OnboardingCarousel from '../onboarding/OnboardingCarousel';
 import { TokensQuery, TokensQueryVariables } from '../../queries-thegraph/types-thegraph/TokensQuery';
-import { Header } from 'semantic-ui-react';
+import { Header, Image } from 'semantic-ui-react';
 import { Link, useNavigate } from 'react-router-dom';
 import { aboutRoute } from '../../routingUtils';
 import { ALL_CATEGORIES_VALUE, TokenCategoryDropdown } from '../TokenCategoryDropdown';
 import { projectId } from '../../utils';
+import streamr_logo from '../../images/streamr_simple.svg'
+import { SocialIcon } from 'react-social-icons';
 
 const Home = () => {
 
@@ -29,6 +31,11 @@ const Home = () => {
             <OnboardingCarousel/>
             <div style={{'textAlign': 'left', padding: '5vh 10vw 7vh 10vw'}}>
                 <Header className="No-overflow" as='h1'>Contribution awards to the Streamr community</Header>
+                <div>
+                    <Image src={streamr_logo} size='mini' style={{"display":"inline-block"}} as='a' href='https://streamr.network/'/>
+                    <SocialIcon url="https://discord.gg/gZAm8P7hK8" label="Streamr Discord" style={{"height":"35px","width":"35px",'marginLeft':'15px'}} />
+                    <SocialIcon url="https://twitter.com/streamr" label="Streamr Twitter" style={{"height":"35px","width":"35px",'marginLeft':'15px'}}/>
+                </div>
                 <p className='projectExplanation'>Streamr community award tokens (sNFT’s) are minted to community members to acknowledge their valuable contributions to the project and community. Read more <Link to={aboutRoute} onClick={() => navigate(aboutRoute)}>about this service.</Link></p>
                 <TokenCategoryDropdown onCategoryChanged={(category) => setSelectedCategory(category)}/>
             </div>
