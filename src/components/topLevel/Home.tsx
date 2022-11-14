8adea45d9a90f1818f6539df06f0fddd06d8d4cd
yaruno <jarno.t.marttila@gmail.com>
2022-11-14 10:03:49 +0100

    Feature/social media links (#64)
    * set desktop view to 3 columns and set max width for grid not to stretch too much on large screens
    
    * adjust homepage padding
    
    * adjusted card sizes, adjusted layout depending on viewport sizes, very large displays will see white bars around the main content
    
    * Adjustments to carousel padding & margiins
    
    * Added social media links to card view (share to socials), added affordance polygon logo in relation to token metrics
    
    * Social media links & project link to project page
    
    * Polygon logo removed, adjusted position of Streamr project social media links & logos, adjusted position of token card social media share links
    
    * expanded shorten account anddress util method to take length to shorten to
    
    * Revert "expanded shorten account anddress util method to take length to shorten to"
    
    This reverts commit a2a07b6c39f6928650537649b0a80e1d9ade7913.
    
    Co-authored-by: Martin Moravek <martin.m.moravek@gmail.com>

--- src/components/topLevel/Home.tsx
+++ src/components/topLevel/Home.tsx
@@ -5,11 +5,13 @@ import { theGraphApolloClient } from '../../graphql/theGraphApolloClient';
 import { defaultErrorHandler } from '../../graphql/errorHandlers';
 import TokenGrid from '../../components/TokenGrid';
 import { TokensQuery, TokensQueryVariables } from '../../queries-thegraph/types-thegraph/TokensQuery';
-import { Header } from 'semantic-ui-react';
+import { Header, Image } from 'semantic-ui-react';
 import { Link, useNavigate } from 'react-router-dom';
 import { aboutRoute } from '../../routingUtils';
 import { ALL_CATEGORIES_VALUE, TokenCategoryDropdown } from '../TokenCategoryDropdown';
 import { projectId } from '../../utils';
+import streamr_logo from '../../images/streamr_simple.svg'
+import { SocialIcon } from 'react-social-icons';
 
 const Home = () => {
 
@@ -27,7 +29,12 @@ const Home = () => {
         <div>
             <div style={{'textAlign': 'left', padding: '5vh 10vw 7vh 10vw'}}>
                 <Header className="No-overflow" as='h1'>Contribution awards to the Streamr community</Header>
-                <p style={{ padding: '3vh 30vw 3vh 0vw'}}>Streamr community award tokens (sNFT’s) are minted to community members to acknowledge their valuable contributions to the project and community. Read more <Link to={aboutRoute} onClick={() => navigate(aboutRoute)}>about this service.</Link></p>
+                <p className='projectExplanation'>Streamr community award tokens (sNFT’s) are minted to community members to acknowledge their valuable contributions to the project and community. Read more <Link to={aboutRoute} onClick={() => navigate(aboutRoute)}>about this service.</Link></p>
+                <div style={{'marginBottom': '2em'}}>
+                    <Image src={streamr_logo} size='mini' style={{"display":"inline-block"}} as='a' href='https://streamr.network/'/>
+                    <SocialIcon url="https://discord.gg/gZAm8P7hK8" label="Streamr Discord" style={{"height":"35px","width":"35px",'marginLeft':'15px'}} />
+                    <SocialIcon url="https://twitter.com/streamr" label="Streamr Twitter" style={{"height":"35px","width":"35px",'marginLeft':'15px'}}/>
+                </div>
                 <TokenCategoryDropdown onCategoryChanged={(category) => setSelectedCategory(category)}/>
             </div>
             
